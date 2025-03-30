70c1b3ebe7e3e3ca5b2e00dc1b5ef0f7998f68a5
Miroshnik-Mariya <mariya3027545@gmail.com>
2025-03-30 21:10:26 +0400

    Лабораторная работа №4
    Компьютерная графика. Сданный вариант.

---
+++ ComputerGrafics_LR4/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace ComputerGrafics_LR4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.ExportBtn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Grey_Btn = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.LinearContrast_btn = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.Filter_btn = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(84, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(404, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Выполнила студентка группы 6201-020302D Мирошник Мария";
+            // 
+            // ExportBtn
+            // 
+            this.ExportBtn.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ExportBtn.Location = new System.Drawing.Point(48, 144);
+            this.ExportBtn.Name = "ExportBtn";
+            this.ExportBtn.Size = new System.Drawing.Size(200, 42);
+            this.ExportBtn.TabIndex = 1;
+            this.ExportBtn.Text = "Загрузить изображение";
+            this.ExportBtn.UseVisualStyleBackColor = false;
+            this.ExportBtn.Click += new System.EventHandler(this.ExportBtn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(298, 72);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(332, 218);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Grey_Btn
+            // 
+            this.Grey_Btn.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Grey_Btn.Location = new System.Drawing.Point(691, 144);
+            this.Grey_Btn.Name = "Grey_Btn";
+            this.Grey_Btn.Size = new System.Drawing.Size(200, 42);
+            this.Grey_Btn.TabIndex = 3;
+            this.Grey_Btn.Text = "Попробовать";
+            this.Grey_Btn.UseVisualStyleBackColor = false;
+            this.Grey_Btn.Click += new System.EventHandler(this.Grey_Btn_Click_1);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(934, 72);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(332, 218);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(162, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Исходное изображение";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(688, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Оттенки серого";
+            // 
+            // LinearContrast_btn
+            // 
+            this.LinearContrast_btn.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.LinearContrast_btn.Location = new System.Drawing.Point(48, 580);
+            this.LinearContrast_btn.Name = "LinearContrast_btn";
+            this.LinearContrast_btn.Size = new System.Drawing.Size(200, 42);
+            this.LinearContrast_btn.TabIndex = 7;
+            this.LinearContrast_btn.Text = "Попробовать";
+            this.LinearContrast_btn.UseVisualStyleBackColor = false;
+            this.LinearContrast_btn.Click += new System.EventHandler(this.LinearContrast_btn_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(298, 404);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(332, 218);
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(36, 371);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(416, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Линейное контрастирование малоконтрастного изображения";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(934, 404);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(332, 218);
+            this.pictureBox4.TabIndex = 10;
+            this.pictureBox4.TabStop = false;
+            // 
+            // Filter_btn
+            // 
+            this.Filter_btn.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Filter_btn.Location = new System.Drawing.Point(691, 463);
+            this.Filter_btn.Name = "Filter_btn";
+            this.Filter_btn.Size = new System.Drawing.Size(200, 42);
+            this.Filter_btn.TabIndex = 11;
+            this.Filter_btn.Text = "Попробовать";
+            this.Filter_btn.UseVisualStyleBackColor = false;
+            this.Filter_btn.Click += new System.EventHandler(this.Filter_btn_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(670, 433);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(233, 16);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Фильтр повышения контрастности";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(36, 404);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(137, 16);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Введите от 1 до 100:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(36, 443);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(125, 16);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Нижнюю границу: ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(36, 514);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(129, 16);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Верхнюю границу: ";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(39, 472);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(11, 16);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "-";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(39, 544);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(11, 16);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1434, 643);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Filter_btn);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.LinearContrast_btn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.Grey_Btn);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.ExportBtn);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ExportBtn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Grey_Btn;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button LinearContrast_btn;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Button Filter_btn;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ ComputerGrafics_LR4/Form1.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ComputerGrafics_LR4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Bitmap bitmap;
+        System.Drawing.Image image;
+        int[,] brightnessMatrix; //матрица яркостей
+
+       
+
+
+        private void ExportBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open_dialog = new OpenFileDialog();
+            open_dialog.Filter = ("Image Files(*.BMP;*.JPG;*.GIF;*.PNG)|*.BMP;*.JPG;*.GIF;*.PNG|All files (*.*)|*.*"); //формат загружаемого файла
+            if (open_dialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    pictureBox1.Image = new Bitmap(open_dialog.FileName);
+                    image = pictureBox1.Image;
+                    Bitmap source = new Bitmap(pictureBox1.Image, pictureBox1.Width, pictureBox1.Height);
+                    pictureBox1.Image = source;
+                    bitmap = new Bitmap(image);
+                    brightnessMatrix = new int[image.Height, image.Width];
+                    FormImageMatrix();
+                }
+                catch
+                {
+                    MessageBox.Show("Невозможно открыть выбранный файл");
+                }
+            }
+
+        }
+
+
+
+
+
+        private void FormImageMatrix() //метод формирования матрицы яркостей
+        {
+            for (int y = 0; y < image.Height; y++)
+            {
+                for (int x = 0; x < image.Width; x++)
+                {
+                    Color c = bitmap.GetPixel(x, y);
+                    int r = Convert.ToInt32(c.R);
+                    int b = Convert.ToInt32(c.B);
+                    int g = Convert.ToInt32(c.G);
+                    int brit = Convert.ToInt32(0.3 * r + 0.59 * g + 0.11 * b);
+                    brightnessMatrix[y, x] = Convert.ToByte(brit);
+                }
+            }
+        }
+
+
+
+        //преобразование в оттенки серого 
+        private Bitmap ConvertToGrey(Bitmap grid)
+        {
+            int w = grid.Width;
+            int h = grid.Height;
+            Color color;
+            for (int y = 0; y < h; y++)
+            {
+                for (int x = 0; x < w; x++)
+                {
+                    color = grid.GetPixel(x, y);
+                    int avg = (int)(0.3 * color.R + 0.59 * color.G + 0.11 * color.B);
+                    grid.SetPixel(x, y, Color.FromArgb(avg, avg, avg));
+                }
+            }
+            return grid;
+        }
+
+        /*
+        private Bitmap LinearContrast(Bitmap grid)
+        {
+            int width = grid.Width;
+            int height = grid.Height;
+            int p1 = int.Parse(numericUpDown1.Text);
+            int p2 = int.Parse(numericUpDown2.Text);
+
+            if (p1>=1 && p2>=1 && p1<=100 && p2<=100) { }
+            else 
+            { }
+            // Создаем массивы для хранения значений яркости пикселей
+            int[] grayScaleValues = new int[width * height];
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Color color = grid.GetPixel(x, y);
+                    int avg = (int)(0.3 * color.R + 0.59 * color.G + 0.11 * color.B);
+                    grayScaleValues[y * width + x] = avg;
+                }
+            }
+
+            Array.Sort(grayScaleValues);
+
+            // Вычисляем минимальное и максимальное значения, используя проценты 
+            int minGray = grayScaleValues[(int)(p1 / 100.0 * (width * height - 1))];
+            int maxGray = grayScaleValues[(int)(p2 / 100.0 * (width * height - 1))];
+
+            minGray = Math.Max(0, Math.Min(255, minGray));
+            maxGray = Math.Max(0, Math.Min(255, maxGray));
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Color originalColor = grid.GetPixel(x, y);
+
+                    float newRed = (float)(originalColor.R - minGray) / (maxGray - minGray) * 255;
+                    float newGreen = (float)(originalColor.G - minGray) / (maxGray - minGray) * 255;
+                    float newBlue = (float)(originalColor.B - minGray) / (maxGray - minGray) * 255;
+
+                    newRed = Math.Max(0, Math.Min(255, newRed));
+                    newGreen = Math.Max(0, Math.Min(255, newGreen));
+                    newBlue = Math.Max(0, Math.Min(255, newBlue));
+
+                    grid.SetPixel(x, y, Color.FromArgb((int)newRed, (int)newGreen, (int)newBlue));
+                }
+            }
+
+            return grid;
+        }
+        */
+
+
+        private Bitmap LinearContrast(Bitmap grid, double lowPercentile = 1.0, double highPercentile = 99.0)
+        {
+            int width = grid.Width;
+            int height = grid.Height;
+
+            int[] grayScaleValues = new int[width * height];
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Color color = grid.GetPixel(x, y);
+                    int avg = (int)(0.3 * color.R + 0.59 * color.G + 0.11 * color.B);
+                    grayScaleValues[y * width + x] = avg;
+                }
+            }
+
+            Array.Sort(grayScaleValues);
+            int minGray = grayScaleValues[(int)(lowPercentile / 100.0 * (width * height - 1))];
+            int maxGray = grayScaleValues[(int)(highPercentile / 100.0 * (width * height - 1))];
+
+            minGray = Math.Max(0, Math.Min(255, minGray));
+            maxGray = Math.Max(0, Math.Min(255, maxGray));
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Color originalColor = grid.GetPixel(x, y);
+                    float newRed = (float)(originalColor.R - minGray) / (maxGray - minGray) * 255;
+                    float newGreen = (float)(originalColor.G - minGray) / (maxGray - minGray) * 255;
+                    float newBlue = (float)(originalColor.B - minGray) / (maxGray - minGray) * 255;
+
+                    newRed = Math.Max(0, Math.Min(255, newRed));
+                    newGreen = Math.Max(0, Math.Min(255, newGreen));
+                    newBlue = Math.Max(0, Math.Min(255, newBlue));
+
+                    grid.SetPixel(x, y, Color.FromArgb((int)newRed, (int)newGreen, (int)newBlue));
+                }
+            }
+            label9.Text = Convert.ToString(minGray);
+            label10.Text = Convert.ToString(maxGray);
+            return grid;
+        }
+
+        public static Bitmap HighPassFilter(Bitmap grid)
+        {
+            int width = grid.Width;
+            int height = grid.Height;
+
+            // Маска фильтра повышения контрастности
+            int[,] filterMatrix = new int[,]
+            {
+            {-1, -1, -1},
+            {-1,  9, -1},
+            {-1, -1, -1}
+            };
+
+            // Создаем временную копию для хранения новых значений пикселей
+            Color[,] tempPixels = new Color[width, height];
+
+            // Обработка пикселей
+            for (int y = 1; y < height - 1; y++)
+            {
+                for (int x = 1; x < width - 1; x++)
+                {
+                    int newRed = 0, newGreen = 0, newBlue = 0;
+
+                    // Применяем маску фильтра
+                    for (int i = -1; i <= 1; i++)
+                    {
+                        for (int j = -1; j <= 1; j++)
+                        {
+                            int neighborX = x + j;
+                            int neighborY = y + i;
+
+                            // Проверяем, что соседние пиксели находятся в пределах границ
+                            if (neighborX >= 0 && neighborX < width && neighborY >= 0 && neighborY < height)
+                            {
+                                // Получаем цвет соседнего пикселя
+                                Color neighborPixel = grid.GetPixel(neighborX, neighborY);
+
+                                // Применяем веса из маски
+                                newBlue += neighborPixel.B * filterMatrix[i + 1, j + 1];
+                                newGreen += neighborPixel.G * filterMatrix[i + 1, j + 1];
+                                newRed += neighborPixel.R * filterMatrix[i + 1, j + 1];
+                            }
+                        }
+                    }
+
+                    // Ограничиваем значения в диапазоне 0-255
+                    newRed = Math.Max(0, Math.Min(255, newRed));
+                    newGreen = Math.Max(0, Math.Min(255, newGreen));
+                    newBlue = Math.Max(0, Math.Min(255, newBlue));
+
+                    // Сохраняем новое значение во временном массиве
+                    tempPixels[x, y] = Color.FromArgb(newRed, newGreen, newBlue);
+                }
+            }
+
+            // Применяем новые значения из временного массива к изображению
+            for (int y = 1; y < height - 1; y++)
+            {
+                for (int x = 1; x < width - 1; x++)
+                {
+                    grid.SetPixel(x, y, tempPixels[x, y]);
+                }
+            }
+
+            // Обработка краев: копируем значения с соседних пикселей
+            // Верхняя и нижняя границы
+            for (int x = 0; x < width; x++)
+            {
+                grid.SetPixel(x, 0, grid.GetPixel(x, 1));
+                grid.SetPixel(x, height - 1, grid.GetPixel(x, height - 2));
+            }
+
+            // Левая и правая границы
+            for (int y = 0; y < height; y++)
+            {
+                grid.SetPixel(0, y, grid.GetPixel(1, y));
+                grid.SetPixel(width - 1, y, grid.GetPixel(width - 2, y));
+            }
+
+            return grid;
+        }
+    
+
+
+
+        private void Grey_Btn_Click_1(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Image, pictureBox1.Width, pictureBox1.Height);
+            bmp = ConvertToGrey(bmp);
+            pictureBox2.Image = bmp;
+
+        }
+
+        private void LinearContrast_btn_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(pictureBox2.Image, pictureBox2.Width, pictureBox2.Height);
+            bmp = LinearContrast(bmp);
+            pictureBox3.Image = bmp;
+
+        }
+
+        private void Filter_btn_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(pictureBox2.Image, pictureBox2.Width, pictureBox2.Height);
+            bmp = HighPassFilter(bmp);
+            pictureBox4.Image = bmp;
+        }
+    }
+}
+
